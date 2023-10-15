67ce0be89b3271bc7ed7d61b0a4bfbce0781ef4d
robin <tmai02476@gmail.com>
2023-10-15 15:13:46 +0200

    code cleaning

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import StyleRSPage2 from "./components/StyleRSPage2";
 import {Button} from "@mui/material";
 
