83170a94609f265bc9b30de4ea2c9bb199c2061d
katharina.stern <katharina.stern@aracom.de>
2023-10-26 22:06:55 +0200

    Create Landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import StyleRSPage2 from "./components/StyleRSPage2";
-import {Button} from "@mui/material";
 import StyleKSHome from "./components/StyleKSHome";
 
 
