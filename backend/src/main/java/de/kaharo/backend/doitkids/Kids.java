bffcbfd450b75130f0922457974f20bb77971abf
Robin <tmai02476@gmail.com>
2023-12-13 16:39:45 +0100

    added name,age,indoor,outdoor,groupSize,sporty and educational to Kids Record.

--- backend/src/main/java/de/kaharo/backend/doitkids/Kids.java
+++ backend/src/main/java/de/kaharo/backend/doitkids/Kids.java
@@ -5,6 +5,15 @@ import org.springframework.data.annotation.Id;
 public record Kids (
 
         @Id
-        String id
+        String id,
+        String name,
+        Integer age,
+        Boolean indoor,
+        Boolean outdoor,
+        Integer groupSize,
+        Boolean sporty,
+        Boolean educational,
+        // Using float for decimal numbers, as we need to represent values like "10,50€"
+        Float cost
 ){
 }
