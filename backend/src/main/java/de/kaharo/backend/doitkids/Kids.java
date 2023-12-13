0401d9d90c5bbe305817b1857e1a46354d1f4f60
Robin <tmai02476@gmail.com>
2023-12-13 17:35:37 +0100

    added a Explanation to all the Datatypes

--- backend/src/main/java/de/kaharo/backend/doitkids/Kids.java
+++ backend/src/main/java/de/kaharo/backend/doitkids/Kids.java
@@ -16,4 +16,13 @@ public record Kids (
         // Using float for decimal numbers, as we need to represent values like "10,50€"
         Float cost
 ){
+        // Explanation:
+        // - name: Name of the activity like playground or oen-air bath
+        // - age: Age of the kid.
+        // - indoor: Whether the kid likes indoor activities.
+        // - outdoor: Whether the kid likes outdoor activities.
+        // - groupSize: Depending on the size of the group some activities are not possible.
+        // - sporty: Whether the kid is sporty (can be null if not specified).
+        // - educational: Whether the kid is educational (can be null if not specified).
+        // - cost:When ever an activity should be cost something it's useful to have a Float it could be null also
 }
