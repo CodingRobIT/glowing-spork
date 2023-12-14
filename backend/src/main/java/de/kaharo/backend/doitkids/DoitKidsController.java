76e5588a8bd2559accb82c74ce5fa31fe848cd67
Robin <tmai02476@gmail.com>
2023-12-14 20:58:30 +0100

    added DoitKidsController.java and GetMapping for getAllKidsActivities

---
+++ backend/src/main/java/de/kaharo/backend/doitkids/DoitKidsController.java
@@ -0,0 +1,21 @@
+package de.kaharo.backend.doitkids;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/kidsactivities")
+public class DoitKidsController {
+
+    private final DoitKidsService kidsService;
+
+    @GetMapping
+    public List<KidsActivity> getAllKidsActivities() {
+        return kidsService.getAllKidsActivities();
+    }
+}
