948fb9cddee913b2863ca6e6691ecc1d72d12463
Robin <tmai02476@gmail.com>
2023-12-18 14:23:51 +0100

    update KidsActivity with CommentList

--- backend/src/main/java/de/kaharo/backend/doitkids/KidsActivity.java
+++ backend/src/main/java/de/kaharo/backend/doitkids/KidsActivity.java
@@ -1,22 +1,34 @@
 package de.kaharo.backend.doitkids;
 
+import de.kaharo.backend.doitComments.CommentList;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.annotation.Id;
+
+import java.util.List;
 
 public record KidsActivity(
 
         @Id
         String id,
+        @NotBlank(message = "Name is required")
+        @Size(min = 4, max = 1000)
         String name,
-        Integer age,
+        // should we add Max Age here ?
+        // Byte because it needs less ram and the number is big enough for an minAge
+        Byte minAge,
         Boolean indoor,
         Boolean outdoor,
-        Integer groupSize,
+        Byte groupSize,
         Boolean sporty,
         Boolean educational,
         // Using float for decimal numbers, as we need to represent values like "10,50€"
         Float cost,
-        String location
-){
+        String location,
+        // String descriptionActivity,
+        // now here ist the "import" of CommentList that we could use now for this model
+        List<CommentList> comments
+) {
         // Explanation:
         // - name: Name of the activity, such as playground or open-air bath.
         // - age: Age of the kid.
@@ -28,3 +40,20 @@ public record KidsActivity(
         // - cost: Represents the cost of the activity (can be null if not applicable).
         // - location to get the position of the place
 }
+
+
+// How to use the CommentList here is an explanation:
+
+// for (CommentList comment: kidsActivity.comments()) {
+//    // Zugriff auf den Nutzernamen des Kommentars
+//    System.out.println("User: " + comment.user());
+//
+//    // Zugriff auf das Datum des Kommentars
+//    System.out.println("Datum: " + comment.commentDate());
+//
+//    // Zugriff auf den Kommentartext
+//    System.out.println("Kommentar: " + comment.comment());
+//
+//    // Überprüfen, ob der Kommentar genehmigt wurde
+//    System.out.println("Genehmigt: " + comment.approved());
+//}
