6853b65a4dcbbf1e3386be95fb56424e987e044a
Robin <tmai02476@gmail.com>
2023-12-18 21:06:17 +0100

    updatet App.tsx for get all KidsActivity

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import StyleRSPage2 from "./components/StyleRSPage2";
 import StyleKSHome from "./components/StyleKSHome";
+import KidsActivityGallery from "./components/KidsActivityGallery";
 import Header from "./components/Header";
+import useKidsActivity from "./CustomHooks/useKidsActivity";
+import {useEffect} from "react";
+
 
 
 function App() {
+
+    const {kidsActivity, loadAllKidsActivity} = useKidsActivity();
+
+    useEffect(() => {
+        loadAllKidsActivity();
+        //eslint-disable-next-line
+    }, []);
 
     return (
         <BrowserRouter>
@@ -13,6 +24,7 @@ function App() {
                 <Routes>
                     <Route path="/page2" element={<StyleRSPage2 />} />
                     <Route path="/" element={<StyleKSHome/>} />
+                    <Route path="/kidsactivity" element={<KidsActivityGallery kidsActivity={kidsActivity}/>} />
                 </Routes>
 
             </div>
