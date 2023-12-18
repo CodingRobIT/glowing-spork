b2aef520c1dbdec09cdf1a3a3c847115f48ac7a9
Robin <tmai02476@gmail.com>
2023-12-18 14:22:48 +0100

    edit DoitKidsService with example Code for get allo with CommentList but don't work

--- backend/src/main/java/de/kaharo/backend/doitkids/DoitKidsService.java
+++ backend/src/main/java/de/kaharo/backend/doitkids/DoitKidsService.java
@@ -1,10 +1,13 @@
 package de.kaharo.backend.doitkids;
 
+import de.kaharo.backend.doitComments.CommentList;
+//import de.kaharo.backend.doitComments.CommentListRepoInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -28,4 +31,20 @@ public class DoitKidsService {
         return activities;
     }
 
+//    public KidsActivity getKidsActivityWithComments(String activityId) {
+//        // Zuerst die KidsActivity anhand der ID finden
+//        Optional<KidsActivity> activityOpt = kidsActivityRepoInterface.findById(activityId);
+//
+//        if (activityOpt.isPresent()) {
+//            KidsActivity activity = activityOpt.get();
+//
+//            // Kommentare, die die gleiche activityId haben, finden
+//            List<CommentList> comments = CommentListRepoInterface.findByActivityId(activityId);
+//
+//            // Erstelle eine neue KidsActivity mit den Kommentaren
+//            return new KidsActivity(activity.id(),activity.name(),activity.minAge(),activity.indoor(),activity.outdoor(), activity.groupSize(), activity.sporty(),activity.educational(),activity.cost(),activity.location(),comments); // Fülle die restlichen Felder entsprechend aus
+//        } else {
+//            throw new NoSuchElementException("KidsActivity with ID " + activityId + " not found");
+//        }
+//    }
 }
